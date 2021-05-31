36bc0beb1baeb3c406cfb86038fa1fc7a6195c01
student <audreymunyiva@gmail.com>
2021-05-31 19:25:40 +0300

    index

--- bank.py
+++ bank.py
@@ -1,10 +1,12 @@
+from datetime import datetime
 class BankAccount:
    
-    def __init__ (self,name,phoneNumber,loan):
+    def __init__ (self,name,phoneNumber):
             self.name=name
             self.balance=0
             self.phoneNumber=phoneNumber
-            self.loan=loan
+            self.loan=0
+            self.statement=[]
     def amount(self):
         return f"Your remaining balance is {self.balance} Thank you for chosing our bank"
 
@@ -20,17 +22,50 @@ class BankAccount:
            return f"Hello {self.name} Your balance is kshs {self.balance}"
        else:
             self.balance += ammount
+            now=datetime.now()
+            transaction={
+                "ammount":{ammount},
+                "time":now,
+                "Narration":" You succesfully deposited"
+            }
+            self.statement.append(transaction)
             return self.show_balance()
 
+    def show_statement(self):
+        for transaction in self.statement:
+           ammount=transaction["ammount"]
+           Narration=transaction["Narration"]
+           time=transaction["time"]
+           date=time.strftime("%d /%m /%y")
+           print(f"{date} {Narration} {ammount}")
+        
+         
+        
+     
     def withdraw(self,ammount):
        if ammount > self.balance:
            return f"Your balance is {self.balance} you can not withdraw {ammount}"
        else:
-           self.balance == ammount
+           self.balance -= ammount
+           now=datetime.now()
+           transaction={
+                "ammount":{ammount},
+                "time":now,
+                "Narration":" You have withdrawn"
+            }
+           self.statement.append(transaction)
+
            return self.show_balance()
+
+
 
     def borrow(self,ammount):
         return f"Hello {self.name} I am glad to inform you that you can now acess kshs {ammount-self.loan} of loan from us"
 
     def pay(self,ammount):
         return f"Hello you are due to pay our loan of kshs{ammount}" 
+
+    
+# starndard libray you can say import math (contains a lot of math) 
+# from datetime import datetime deals with time
+#to get current time use datetime.now() to format time we use create a var now=datetime.now
