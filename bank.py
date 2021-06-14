66158c72e123794950db0fb7e8a3df21d5d8e635
student <audreymunyiva@gmail.com>
2021-06-14 21:17:56 +0300

    class

--- bank.py
+++ bank.py
@@ -9,18 +9,18 @@ class BankAccount:
             self.statement=[]
     def amount(self):
         return f"Your remaining balance is {self.balance} Thank you for chosing our bank"
-
-    def name(self):
-        return f"{self.name} bank is my bank of choice "
-
-
+        
     def show_balance(self):
         return f"Hello {self.name}, your balance is {self.balance}"
 
     def deposit(self,ammount):
-       if ammount<0:
+        try:
+            0+ammount
+        except TypeError:
+            return f"The ammount must be in figures"
+        if ammount<0:
            return f"Hello {self.name} Your balance is kshs {self.balance}"
-       else:
+        else:
             self.balance += ammount
             now=datetime.now()
             transaction={
@@ -30,6 +30,20 @@ class BankAccount:
             }
             self.statement.append(transaction)
             return self.show_balance()
+
+    def transfer(self,account,ammount):
+            try:
+                0+ammount
+            except TypeError:
+                return f"Please input your ammount in figures"
+            fee= ammount*0.05
+            total=ammount+ fee
+            self.balance-=total
+            account.deposit(ammount)
+            if total>self.balance:
+                    return f"Hello {self.name} your balance is {self.balance} and you need at least {total} for this transfer"
+            else :
+                    return f"Hello {self.name} you have transfered your money to a different account"
 
     def show_statement(self):
         for transaction in self.statement:
@@ -43,9 +57,13 @@ class BankAccount:
         
      
     def withdraw(self,ammount):
-       if ammount > self.balance:
+        try:
+            0+ammount
+        except TypeError:
+            return f"The ammount must be in figures"
+        if ammount > self.balance:
            return f"Your balance is {self.balance} you can not withdraw {ammount}"
-       else:
+        else:
            self.balance -= ammount
            now=datetime.now()
            transaction={
@@ -60,6 +78,10 @@ class BankAccount:
 
 
     def borrow(self,ammount):
+        try:
+            0+ammount
+        except TypeError:
+            return f"Please input the ammount in figures"
         if ammount < 0:
             return f"Your amount is{self.balance} you can not borrow a loan"
         elif  self.loan > 0:
@@ -84,6 +106,10 @@ class BankAccount:
             
 
     def repay(self,ammount):
+        try:
+            0+ammount
+        except TypeError:
+            return f"Please input the ammount in figures"
         if ammount <0:
            return f"Hello {self.name} you should repay your entire loan"
         elif ammount<= self.loan:
@@ -102,6 +128,29 @@ class BankAccount:
             self.statement.append(transaction)
  
             return f"You have succesfuly repaid your loan" 
+
+class MobileMoneyAccount(BankAccount):
+    def __init__(self, name, phoneNumber,service_provider):
+        BankAccount.__init__(self,name, phoneNumber)
+        self.service_provider=service_provider
+
+    def buy_airtime(self,ammount):
+        try:
+            0+ammount
+        except TypeError:
+            return f"Please input the ammount in figures"
+        if ammount<0:
+            return f"Hello {self.name} your balance is {ammount}"
+        elif self.balance < ammount:
+            return f"Dear customer your account balance is low recharge your balance"
+        else :
+            self.balance -=ammount
+            return f"Dear customer you have bought airtime worth {ammount}"
+    
+    
+
+        
+      
       
      
 
